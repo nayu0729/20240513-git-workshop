222b132d0c3c812aace80787e720cc00c96daa28
nayu0729 <231205023@ccmailg.meijo-u.ac.jp>
2024-05-13 15:36:13 +0900

    ピースの色を変更
    明るい緑色に変更

--- js/tetris.js
+++ js/tetris.js
@@ -7,9 +7,9 @@ var current; // current moving shape
 var currentX, currentY; // position of current shape
 var freezed; // is current shape settled on the board?
 var shapes = [
-    [ 1, 1, 1, 1 ],
-    [ 1, 1, 1, 0,
-      1 ],
+    [ 1, 1, 1, 1 ,1,1,1,1], // 1,1,1,1,
+    [ 1, 1, 1, 1, // 1,1,1,0,1
+      1 ,0,1,0],
     [ 1, 1, 1, 0,
       0, 0, 1 ],
     [ 1, 1, 0, 0,
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'green', 'purple'
+    'cyan', 'orange', 'blue', 'yellow', 'red', 'lime', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
